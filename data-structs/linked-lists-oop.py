bc7ef95eeebdadd2c62c3ae04654306b80a607df
Toby Hogan <53061712+KrypticSilver@users.noreply.github.com>
2022-03-06 09:38:28 +0000

    Finished insertion sort

--- data-structs/linked-lists-oop.py
+++ data-structs/linked-lists-oop.py
@@ -1,46 +1,9 @@
-# class Node:
-#     def __init__(self, data: int):
-#         self.data = data
-#         self.next = None
-
-
-# class LinkedList:
-#     def __init__(self, data: list = None):
-#         self.data = data
-#         if self.data:
-#             self.head = None
-
-#         else:
-#             self.head = data[1]
-
-#     def insert_item(self, item, position):
-#         self.data.insert(position, item)
-
-#     def add_item(self, item):
-#         self.data.append(item)
-
-#     def display_list(self):
-#         print(self.data)
-
-#     def get_iterator(self):
-#         return self.data
-
-#     def del_item(self):
-
-
-# my_list = LinkedList([1, 3, 2])
-# my_list.add_item(2)
-# my_list.insert_item(3, 3)
-# my_list.display_list()
-# for i in my_list.get_iterator():
-#     print(i)
-
 
 class Node:
-    def __init__(self, data=None, pos=None, next=None):
+    def __init__(self, data=None, pos=None, =None):
         self.data = data
         self.pos = pos
-        self.next = next
+        self.next = None
 
 
 class LinkedList:
--- sort-algos-28-feb/insertion-sort.py
+++ sort-algos-28-feb/insertion-sort.py
@@ -1,28 +1,32 @@
 # This is the unsorted data set that will be sorted
 data = [81, 2, 99, 22, 33, 45, 55, 29]
-<<<<<<< HEAD
-
-# creates a list variable that will contain the sorted items
-sorted_data = [0]
-
-for i in data[0:-1]:
-    for e, j in enumerate(sorted_data):
-        if i > j:
-            sorted_data.insert((e + 1), i)
-            print(sorted_data)
-
-print(sorted_data)
-    
 
 
+def lazy_insertion(data):
+    # creates a list variable that will contain the sorted items
+    sorted_data = []
+    for i in range(len(data)):
+        sorted_data.append(min(data))
+        data.remove(min(data))
 
-=======
-sorted_data = [None for i in data]
-
-for e, i in enumerate(range(len(data), 0, -1)):
-    if i > sorted_data[e]:
-        sorted_data.append(i)
+    return sorted_data
 
 
-print(sorted_data)
->>>>>>> bc4daa0ed3a53cc35766df0312d3e40df39e20b2
+def hard_insertion(data):
+    # creates a list variable that will contain the sorted items
+    sorted_data = [0]
+    for i in data:
+        for e, j in enumerate(sorted_data):
+            if len(sorted_data) <= (e + 1):
+                if (i > j):
+                    sorted_data.insert(e + 1, i)
+
+            elif (i > j) and (i < sorted_data[e + 1]):
+                sorted_data.insert(e + 1, i)
+
+    sorted_data.remove(0)
+    print(sorted_data)
+    return sorted_data
+
+
+print(hard_insertion(data))

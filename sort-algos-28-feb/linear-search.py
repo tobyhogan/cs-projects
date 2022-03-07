1019d2e2cc087bb8780dc882d7730eddaa12e686
Toby Hogan <53061712+KrypticSilver@users.noreply.github.com>
2022-03-07 19:30:13 +0000

    Commented on linear search.

--- sort-algos-28-feb/linear-search.py
+++ sort-algos-28-feb/linear-search.py
@@ -1,10 +1,16 @@
+# this is the data set that I will be using as an example to find an item in
 data = [1, 8, 1, 2, 9, 7, 6, 3, 5]
 
-
+# this is the function definition where I get the item and data parameter, item is what I want to find, data is what I want to look for it in
 def linear_search(item, data):
+    # iterates through data, also getting an index as it goes
     for e, i in enumerate(data):
+        # checks if the current item is equal to the item the search is looking for
         if i == item:
+            # if it does find the item, aka they match then it will announce it 
             print(f"Found at index [{e}]")
+            # once the item is found, it will also stop the search as it no longer needs to continue
+            break
 
-
+# calling the function
 linear_search(5, data)

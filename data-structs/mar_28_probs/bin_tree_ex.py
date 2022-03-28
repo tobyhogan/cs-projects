2da5a5af4e8c2695e2bf41e0d3bbc0a06d6b777a
Toby Hogan <53061712+KrypticSilver@users.noreply.github.com>
2022-03-28 20:14:03 +0100

    .

--- data-structs/mar_28_probs/bin_tree_ex.py
+++ data-structs/mar_28_probs/bin_tree_ex.py
@@ -2,7 +2,7 @@ import time
 
 # an example where a binary tree could be used is in a family (i.e. where each family member has 2 children)
 
-#
+ln_break = "============================================================================"
 
 
 class Node:
@@ -11,6 +11,7 @@ class Node:
         self.parent = None
         self.right = None
         self.left = None
+        self.depth = None
 
 
 class BinaryTree:
@@ -23,6 +24,7 @@ class BinaryTree:
         # if the item to be added is the first item, make it the root
         if self.root == None:
             self.root = item
+            self.root.depth = 0
 
         # if the item to be added is not the first item, make the root the pointer
         else:
@@ -31,14 +33,17 @@ class BinaryTree:
             # go through each node that decends off of the root node until a space is found
             while True:
                 # if the current location of the pointer's name is higher in the alphabet than the name of the item then try to make it go to the right
-                if self.pointer.name >= item.name:
-                    # if the right has nothign on it, then make the node that location(on the right)
-
-                    print(item.name, "Going right")
+                if self.pointer.name <= item.name:
+                    # if the right has nothing on it, then make the node that location(on the right)
 
                     if self.pointer.right == None:
+                        # making the place to the right of the pointer equal to the item
                         self.pointer.right = item
-                        print(item.name, "Placed")
+                        # making the parent of the item, the pointer
+                        item.parent = self.pointer
+                        # making the depth of the item the depth of the parent add 1
+                        item.depth = self.pointer.depth + 1
+
                         break
 
                     else:
@@ -46,13 +51,17 @@ class BinaryTree:
                         self.pointer = self.pointer.right
 
                 # if the items name is higher in the alphabet than the current node then try and make it go to the left
-                elif item.name > self.pointer.name:
-                    print(item.name, "Going left")
+                elif item.name < self.pointer.name:
 
                     # if the item on the left is empty then make it hold the item
                     if self.pointer.left == None:
+                        # making the place to the left of the pointer equal the item
                         self.pointer.left = item
-                        print(item.name, "Placed")
+                        # making the parent of the item, the pointer
+                        item.parent = self.pointer
+                        # making the depth of the item the depth of the parent add 1
+                        item.depth = self.pointer.depth + 1
+
                         break
 
                     else:
@@ -60,49 +69,101 @@ class BinaryTree:
                         self.pointer = self.pointer.left
 
     def display_as_tree(self):
-        nodes = [(0, self.root)]
+        # making a list of nodes to contain all the other nodes, and the depth they're at
+        nodes = [self.root]
 
-        if nodes[0][1].left != None:
-            nodes.append((1, nodes[0][1].left))
+        # checking if the root node has a left node, if it does: add it to the list at depth 1
+        if self.root.left != None:
+            nodes.append(self.root.left)
 
-        if nodes[0][1].right != None:
-            nodes.append((1, nodes[0][1].right))
+        # checking if the root node has a right node, if it does: add it to the list at depth 1
+        if self.root.right != None:
+            nodes.append(self.root.right)
 
-        level = 1
+        # setting the default depth to be 1, as we've just added two items at level 1
+        depth = 1
 
+        # starting an infinite loop to go through and explore each node to see if it has other nodes
         while True:
+            # setting the value of the updated variable to false(a.k.a no new nodes have been added YET)
             updated = False
-            print("Level in while loop", level)
 
+            # going through each of the nodes in the list and looking to see if they have children
             for i in nodes:
-                if not(i in nodes):
+                # checking to see if the index of the item has the greatest current depth(just the depth)
+                if i.depth == depth:
+                    # if the left child of the node is not empty, add that node to the list
                     if i.left != None:
-                        nodes.append((level, i.left))
+                        nodes.append(i.left)
                         updated = True
 
                     if i.right != None:
-                        nodes.append((level, i.right))
+                        nodes.append(i.right)
                         updated = True
 
-            level += 1
+            depth += 1
 
             if updated == False:
                 break
 
-        print(nodes)
+        print(ln_break)
+        for i in range(depth):
+            print("Depth", i)
+            print([j.name for j in nodes if j.depth == i])
 
-        for i in range(level):
-            print("Level", i)
-            print([item[1].name for item in nodes if item[0] == i])
+        print(ln_break)
 
     def display_as_list(self):
-        self.ponter = root
+        # making a list of nodes to contain all the other nodes, and the depth they're at
+        nodes = [self.root]
+
+        # checking if the root node has a left node, if it does: add it to the list at depth 1
+        if self.root.left != None:
+            nodes.append(self.root.left)
+
+        # checking if the root node has a right node, if it does: add it to the list at depth 1
+        if self.root.right != None:
+            nodes.append(self.root.right)
+
+        # setting the default depth to be 1, as we've just added two items at level 1
+        depth = 1
+
+        # starting an infinite loop to go through and explore each node to see if it has other nodes
+        while True:
+            # setting the value of the updated variable to false(a.k.a no new nodes have been added YET)
+            updated = False
+
+            # going through each of the nodes in the list and looking to see if they have children
+            for i in nodes:
+                # checking to see if the index of the item has the greatest current depth(just the depth)
+                if i.depth == depth:
+                    # if the left child of the node is not empty, add that node to the list
+                    if i.left != None:
+                        nodes.append(i.left)
+                        updated = True
+
+                    if i.right != None:
+                        nodes.append(i.right)
+                        updated = True
+
+            depth += 1
+
+            if updated == False:
+                break
+
+            print(ln_break)
+            print([i.name for i in nodes])
+            print(ln_break)
 
 
 my_tree = BinaryTree()
 
 my_tree.add_item(Node("Jaketh"))
+my_tree.add_item(Node("Aaron"))
+my_tree.add_item(Node("Adam"))
 my_tree.add_item(Node("Toby"))
-my_tree.add_item(Node("Joby"))
+my_tree.add_item(Node("Jed"))
+
 
 my_tree.display_as_tree()
+my_tree.display_as_list()

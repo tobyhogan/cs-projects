b0512b81db37a30806a088a616c21b4727ad7aa1
Toby Hogan <tobyhogan44@gmail.com>
2022-03-28 09:57:53 +0100

    .

---
+++ data-structs/mar_28_probs/bin_tree_ex.py
@@ -0,0 +1,31 @@
+# an example where a binary tree could be used is in a family (i.e. where each family member has 2 children)
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.parent = None
+        self.right_child = None
+        self.left_child = None
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+        self.pointer = None
+
+    # adds the item depending on the letters in its name
+    def add_item(self, item):
+        if self.root == None:
+            self.root = item
+
+        else:
+            self.pointer = self.root
+
+            while True:
+                for i in range(max(len(self.pointer.name), len(item.name))):
+                    print(i)
+
+
+my_tree = BinaryTree()
+my_tree.add_item(Node("Toby"))
+my_tree.add_item(Node("Jaketh"))
